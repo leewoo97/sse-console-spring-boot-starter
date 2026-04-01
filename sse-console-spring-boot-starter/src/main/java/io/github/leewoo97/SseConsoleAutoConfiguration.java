43f2860da1dfcd09f514badbd1db95dc6d64b599
leewoo97 <dldnwls009@gmail.com>
2026-04-01 10:50:48 +0900

    Revert "README.md 파일작성완료"
    This reverts commit aa1465cd382c1e26235691cd5bed342802767254.

--- sse-console-spring-boot-starter/src/main/java/io/github/leewoo97/SseConsoleAutoConfiguration.java
+++ sse-console-spring-boot-starter/src/main/java/io/github/leewoo97/SseConsoleAutoConfiguration.java
@@ -2,7 +2,6 @@ package io.github.leewoo97;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @AutoConfiguration
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @ConditionalOnClass(RequestMappingHandlerMapping.class)
-@ConditionalOnProperty(name = "sse.console.enabled", havingValue = "true")
 @Import(SseConsoleMetaController.class)
 public class SseConsoleAutoConfiguration {
 }
